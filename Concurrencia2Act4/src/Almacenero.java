e38fc601172ddb1ec9156b5cc10b6266a9b8e21b
David Hernández <david.hmhernandez@gmail.com>
2014-10-20 13:31:14 +0200

    Modificado

--- Concurrencia2Act4/src/Almacenero.java
+++ Concurrencia2Act4/src/Almacenero.java
@@ -14,13 +14,11 @@ public class Almacenero extends Thread {
 		for (;;) {
 			synchronized (a) {
 				if (a.getGalletas() == 0) {
-					System.out
-							.println("Almacenero compruebo el numero de galletas");
+					System.out.println("Almacenero compruebo el numero de galletas");
 					if (a.getGalletas() == 0) {
 						try {
 							a.reponer();
-							System.out
-									.println("Almacenero Salgo del almacen 1/2 segundo ");
+							System.out.println("Almacenero Salgo del almacen 1/2 segundo ");
 							a.wait(500);
 						} catch (InterruptedException ex) {
 							Logger.getLogger(Almacenero.class.getName()).log(
