98070d3c493c525cfeb66fcfde7f23baa17555cf
David Hernández <david.hmhernandez@gmail.com>
2014-10-20 13:45:24 +0200

    Corregido Final

--- Concurrencia2Act1/src/Main.java
+++ Concurrencia2Act1/src/Main.java
@@ -13,8 +13,15 @@ public class Main {
 		hilo1.start();
 		hilo2.start();
 		
-		
+		try {
+			hilo1.join();
+			hilo2.join();
 
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
